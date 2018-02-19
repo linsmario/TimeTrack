90c24a67752d1e5b41f5f2a333c5432ced29f3ec
Emre Sevki Kaya <emre.sevki.kaya.2@gmail.com>
2018-02-19 11:49:49 +0100

    basic test infrastructure

--- app/src/androidTest/java/com/scrum5bwi/timetrack/ControllingAdapter.java
+++ app/src/androidTest/java/com/scrum5bwi/timetrack/ControllingAdapter.java
@@ -3,31 +3,42 @@ package com.scrum5bwi.timetrack;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-import android.support.test.espresso.*;
 
 import org.junit.*;
 import org.junit.runner.RunWith;
-import android.support.test.rule.ActivityTestRule;
 
+import android.support.test.espresso.Espresso;
+import android.widget.Button;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 
 @RunWith(AndroidJUnit4.class)
 public class ControllingAdapter {
 
-    @Rule
-    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
-            MainActivity.class);
+    public int startButton = R.id.buttonLeft;
+    public int stopButton = R.id.buttonRight;
+    public int roundButton;
+    public int time = R.id.time;
 
     @Test
     public void startButton() {
 
-        //onView(withId(startButton)).perform(click());
-        //onView(withId(startButton)).check(matches(withText("00:00")));
+        onView(withId(time)).check(matches(withText("00:00")));
+        onView(withId(startButton)).perform(click());
+
+        try {
+            Thread.sleep(1000);
+            onView(withId(time)).check(matches(withText("00:01")));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
