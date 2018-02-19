1dd9364ac3a51a5d2cbb41c15b98a296a715f4c9
Emre Sevki Kaya <emre.sevki.kaya.2@gmail.com>
2018-02-19 11:12:47 +0100

    setup test

--- app/src/androidTest/java/com/scrum5bwi/timetrack/ControllingAdapter.java
+++ app/src/androidTest/java/com/scrum5bwi/timetrack/ControllingAdapter.java
@@ -3,9 +3,11 @@ package com.scrum5bwi.timetrack;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.espresso.*;
 
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
+import android.support.test.rule.ActivityTestRule;
 
 import static org.junit.Assert.*;
 
@@ -14,13 +16,18 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() throws Exception {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.scrum5bwi.timetrack", appContext.getPackageName());
+@RunWith(AndroidJUnit4.class)
+public class ControllingAdapter {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void startButton() {
+
+        //onView(withId(startButton)).perform(click());
+        //onView(withId(startButton)).check(matches(withText("00:00")));
     }
 }
